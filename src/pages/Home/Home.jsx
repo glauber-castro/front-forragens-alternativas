d96fc2a5be13e49ffc303ca71cec8fd20397d795
Glauber Castro <glaubercold53@gmail.com>
2023-02-02 16:35:23 -0300

    introduzido search

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import "./home.css"
 const Home = () => {
 
     const [plants, setPlants] = useState([]);
+    const [search, setSearch] = useState("")
 
     // listar todas as plantas
     const listPlants = () => {
@@ -14,23 +15,38 @@ const Home = () => {
             })
     }
 
+    const searchPlants = (e) => {
+        setSearch(e.target.value)
+    }
+
     useEffect(() => {
         listPlants()
+        // eslint-disable-next-line
     }, [])
 
     return (
         <div className="container">
-            {plants.map((plant, index) => (
-                <div key={index}>
-                    <div className="positionLayuot">
-                        <img className="imagemPlantas" src={plant.images[0].url} alt={plant.nomePopular} />
-                        <h1 className="title">
-                            {JSON.stringify(plant.nomePopular).toLowerCase().replace(/["]/g, '')}
-                        </h1>
-                    </div>
-                    <hr />
-                </div>
-            ))}
+            <input
+                type="text"
+                placeholder="Digite o nome"
+                onChange={searchPlants}
+            />
+            {plants
+            .filter(plant =>{
+                if(search === "") return plant;
+                else if(plant.nomePopular.toLowerCase().includes(search)) return plant;
+            })
+            .map((plant, index) => (
+                        <div key={index}>
+                            <div className="positionLayuot">
+                                <img className="imagemPlantas" src={plant.images[0].url} alt={plant.nomePopular} />
+                                <h1 className="title">
+                                    {JSON.stringify(plant.nomePopular).toLowerCase().replace(/["]/g, '')}
+                                </h1>
+                            </div>
+                            <hr />
+                        </div>
+                ))}
         </div>
     )
 }
