100cbbd6a09e49f0e0a0430e91991bb624c04a48
Glauber Castro <glaubercold53@gmail.com>
2023-02-28 17:14:21 -0300

    adicionado novos codigos

--- src/pages/Plant/Plant.jsx
+++ src/pages/Plant/Plant.jsx
@@ -1,35 +1,55 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+
+//Component Carrousel 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel"
 
 //Chamando api
 import { api } from "../../utils/api";
+
+//styles
+import "./plant.css"
 
 
 const Plant = () => {
 
     const [single, setSingle] = useState("")
-    const {nomePopular} = useParams();
+    const { nomePopular } = useParams();
 
     //função que chamar uma determinada planta
-    const singlePlant = async () =>{
+    const singlePlant = async () => {
         await api.get(`/planta/${nomePopular}`)
-        .then(res => setSingle(res.data))
-        .catch(error => alert("Error em chamar a planta"))
+            .then(res => setSingle(res.data))
+            .catch(error => alert("Error em chamar a planta"))
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         singlePlant()
         // eslint-disable-next-line
-    },[single])
+    }, [single])
 
-    return(
+    return (
         <div>
-           {single && (
-                <div>
-                    <p>{single.nomePopular}</p>
-                    <p>{single.nomeCientifico}</p>
-                </div>
-           )}
+            {single && (
+                <Carousel
+                    showThumbs={false}
+                    showIndicators={false}
+                    showStatus={false}
+                >
+                    <div className="carroselPosition1">
+                        {single.images.map((image, index) => (
+                               <div className="imagemPosition" key={index}>
+                                     <img src={image.url} alt="" />
+                               </div>
+                        ))}
+                    </div>
+
+                    <div>
+                        ola
+                    </div>
+                </Carousel>
+            )}
         </div>
     )
 }
