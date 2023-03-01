f1bbca2b702bad416a2f3ce3ebd1cf5b2584b2f5
Glauber Castro <glaubercold53@gmail.com>
2023-03-01 17:04:31 -0300

    introduzindo o carrosel

--- src/pages/Plant/Plant.jsx
+++ src/pages/Plant/Plant.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 //Component Carrousel 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -7,6 +7,10 @@ import { Carousel } from "react-responsive-carousel"
 
 //Chamando api
 import { api } from "../../utils/api";
+
+//imagens
+import { AiOutlineClose } from "react-icons/ai";
+import iconFolha from "../../assets/carrousel/icon-folha.jpg";
 
 //styles
 import "./plant.css"
@@ -16,12 +20,18 @@ const Plant = () => {
 
     const [single, setSingle] = useState("")
     const { nomePopular } = useParams();
+    const navigate = useNavigate();
 
     //função que chamar uma determinada planta
     const singlePlant = async () => {
         await api.get(`/planta/${nomePopular}`)
             .then(res => setSingle(res.data))
             .catch(error => alert("Error em chamar a planta"))
+    }
+
+    const closeButton = (e) => {
+        e.preventDefault();
+        navigate("/")
     }
 
     useEffect(() => {
@@ -31,6 +41,13 @@ const Plant = () => {
 
     return (
         <div>
+            <div className="cabecalhoSingle">
+                <img src={iconFolha} alt={single.nomeCientifico} />
+                <div className="positionItems">
+                    <h1 className="singleTitle">{single.nomePopular}</h1>
+                </div>
+                <AiOutlineClose className="styleButtonClose" onClick={closeButton} />
+            </div>
             {single && (
                 <Carousel
                     showThumbs={false}
@@ -39,9 +56,9 @@ const Plant = () => {
                 >
                     <div className="carroselPosition1">
                         {single.images.map((image, index) => (
-                               <div className="imagemPosition" key={index}>
-                                     <img src={image.url} alt="" />
-                               </div>
+                            <div className="imagemPosition" key={index}>
+                                <img src={image.url} alt="" />
+                            </div>
                         ))}
                     </div>
 
